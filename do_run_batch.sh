205d774a671d2ce38b276e1020fee44c46f9b6e0
root <root@ubuntu.qa.local>
2015-07-09 16:48:34 -0600

    Change batch files to reflect new duration oriented parameters

--- do_run_batch.sh
+++ do_run_batch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-./test ngs $3 targets_per 9 chunk_size $2 utilization 0 chunks_per_object $4 objects $5 mbs_sec 500 cluster_trip_time 350 seed 3001 > $1.run1.txt
+./test ngs $3 targets_per 9 chunk_size $2 gateways  $4 mbs_sec 500 cluster_trip_time 350 seed 3001 duration 2000 > $1.run1.txt
 tar zcf $1.run1.tgz $1.run1.txt log.csv bid.csv
-./text ngs $3 targets_per 9 chunk_size $2 utilization 0 chunks_per_object $4 objects $5 mbs_sec 500 cluster_trip_time 350 seed 3002 > $1.run2.txt
+./test ngs $3 targets_per 9 chunk_size $2 gateways  $4 mbs_sec 500 cluster_trip_time 350 seed 3002 duration 2000 > $1.run2.txt
 tar zcf $1.run2.tgz $1.run2.txt log.csv bid.csv
-./test ngs $3 targets_per 9 chunk_size $2  utilization 0 chunks_per_object $4 objects $5 mbs_sec 500 cluster_trip_time 350 seed 3003 > $1.run3.txt
+./test ngs $3 targets_per 9 chunk_size $2 gateways  $4 mbs_sec 500 cluster_trip_time 350 seed 3003 duration 2000 > $1.run3.txt
 tar zcf $1.run3.tgz $1.run3.txt log.csv bid.csv
-./test ngs $3 targets_per 9 chunk_size $2  utilization 0 chunks_per_object $4 objects $5 mbs_sec 500 cluster_trip_time 350 seed 3004 > $1.run4.txt
+./test ngs $3 targets_per 9 chunk_size $2 gateways  $4 mbs_sec 500 cluster_trip_time 350 seed 3004 duration 2000 > $1.run4.txt
 tar zcf $1.run4.tgz $1.run4.txt log.csv bid.csv
-./test ngs $3 targets_per 9 chunk_size $2  utilization 0 chunks_per_object $4 objects $5 mbs_sec 500 cluster_trip_time 350 seed 3005  > $1.run5.txt
+./test ngs $3 targets_per 9 chunk_size $2 gateways  $4 mbs_sec 500 cluster_trip_time 350 seed 3005 duration 2000 > $1.run5.txt
 tar zcf $1.run5.tgz $1.run5.txt log.csv bid.csv
 
--- do_runs.sh
+++ do_runs.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-./do_run_batch.sh t16K4K 4 128 128 128
-./do_run_batch.sh t16K8K 8 128 128 128
-./do_run_batch.sh t16K16K 16 128 128 128
-./do_run_batch.sh t16K128K 128 128 128 128
-./do_run_batch.sh t16K1M 1024 128 128 128
-./do_run_batch.sh t16K16M 16384 128 128 128
-./do_run_batch.sh t32K4K 4 256 128 256
-./do_run_batch.sh t32K8K 8 256 128 256
-./do_run_batch.sh t32K16K 16 256 128 256
-./do_run_batch.sh t32K128K 128 256 128 256
-./do_run_batch.sh t32K1M 1024 256 128 256
-./do_run_batch.sh t32K16M 16384 256 128 256
+./do_run_batch.sh t4K 4 128 256
+./do_run_batch.sh t16K 16 128 256
+./do_run_batch.sh t64K 64 128 256
+./do_run_batch.sh t256K 256 128 256
+./do_run_batch.sh t1M 1024 128 256
+./do_run_batch.sh t4M 4096 128 256
+./do_run_batch.sh t16M 16384 128 256
+
