b6efb3ab5d815cb8c9e9e327f3e0d5ffcdede9f6
root <root@ubuntu.qa.local>
2015-07-10 17:27:49 -0600

    Insert missing $1 parm

--- do_run_batch.sh
+++ do_run_batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./do_1run.sh $2 $3 $4 $5 1
-./do_1run.sh $2 $3 $4 $5 2
-./do_1run.sh $2 $3 $4 $5 3
-./do_1run.sh $2 $3 $4 $5 4
-./do_1run.sh $2 $3 $4 $5 5
+./do_1run.sh $1$2 $3 $4 $5 1
+./do_1run.sh $1 $2 $3 $4 $5 2
+./do_1run.sh $1 $2 $3 $4 $5 3
+./do_1run.sh $1 $2 $3 $4 $5 4
+./do_1run.sh $1 $2 $3 $4 $5 5
