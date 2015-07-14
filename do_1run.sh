f032bcd725dbcb8b4a1b6203d74bc7d4d45da1a0
root <root@ubuntu.qa.local>
2015-07-13 23:32:57 -0600

    script fixes

--- do_1run.sh
+++ do_1run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -v
 ./test terse ngs $3 chunk_size $2 gateways $4 duration 10 seed $5  > $1.run$5.txt
 [ -f "$1.run$5.tgz" ] && rm $1.run$5.tgz
 tar zcf $1.run$5.tgz $1.run$5.txt log.csv bid.csv
