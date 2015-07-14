243ce50db36c74c2a4980a3ba91d3a6491cebe83
cait <User@Macintosh.local>
2015-07-13 17:06:33 -0700

    Update .sh files to rely on defaults and to update parms

--- do_runs.sh
+++ do_runs.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-./do_run_batch.sh t60c4K 4 128 256 60
-./do_run_batch.sh t60c16K 16 128 256 60
-./do_run_batch.sh t60c64K 64 128 256 60
-./do_run_batch.sh t60c256K 256 128 256 60
-./do_run_batch.sh t60c1M 1024 128 256 60
-./do_run_batch.sh t60c4M 4096 128 256 60
-./do_run_batch.sh t60c16M 16384 128 256 60
-./do_run_batch.sh t90c4K 4 128 256 90
-./do_run_batch.sh t90c16K 16 128 256 90
-./do_run_batch.sh t90c64K 64 128 256 90
-./do_run_batch.sh t90c256K 256 128 256 90
-./do_run_batch.sh t90c1M 1024 128 256 90
-./do_run_batch.sh t90c4M 4096 128 256 90
-./do_run_batch.sh t90c16M 16384 128 256 90
+./do_run_batch.sh g64c4K 4 128 64
+./do_run_batch.sh g64c128K 128 128 64
+./do_run_batch.sh g64c2M 2048 128 64
+./do_run_batch.sh g256c4K 4 128 256
+./do_run_batch.sh g256c128K 128 128 256
+./do_run_batch.sh g256c2M 2048 128 256
 
