fbdb071ed03a98a385e96db46e532589296adede
cait <User@Macintosh.local>
2015-07-16 09:04:11 -0700

    remove explicit bwm parm, rely on default

--- do_1run.sh
+++ do_1run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-./test terse ngs $3 chunk_size $2 gateways $4 duration 100 bwm 2 seed $5 | tee $1.run$5.txt
+./test terse ngs $3 chunk_size $2 gateways $4 duration 100 seed $5 | tee $1.run$5.txt
 set +x
 [ -f "$1.run$5.tgz" ] && rm $1.run$5.tgz
 tar zcf $1.run$5.tgz $1.run$5.txt log.csv bid.csv
