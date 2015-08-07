b3f267368347ef6f717350b05746a0aabf65373e
cait <User@Macintosh.local>
2015-08-07 14:16:47 -0700

    Redone for latest command line parms i

--- do_benchmarks.sh
+++ do_benchmarks.sh
@@ -1,51 +1,51 @@
 #!/bin/bash
 #
-# penalty is Replicast chunk processing overhead above TCP connection establishment
-# penalty here is set to 20000 or 2ms, which is 2 times CLUSTER_TRIP_TIME
-# note: (2 * CLUSTER_TRIP_TIME) == RTT
-#
-#############
-name="gw64c4K"
-#############
-[ -f $name.txt ] && rm $name.txt
-set -x
-./test terse gateways 64 chunk_size 4 ngs 128 duration 24 penalty 20000 seed 5791 | tee $name.txt
-set +x
-[ -f $name.tgz ] && rm $name.tgz
-tar zcf $name.tgz $name.txt log.csv
-#############
-name="gw256c4K"
-#############
-[ -f $name.txt ] && rm $name.txt
-set -x
-./test terse gateways 256 chunk_size 4 ngs 128 duration 24 penalty 20000 seed 5791 | tee $name.txt
-set +x
-[ -f $name.tgz ] && rm $name.tgz
-tar zcf $name.tgz $name.txt log.csv
-#############
-name="gw64c128K"
-#############
-[ -f $name.txt ] && rm $name.txt
-set -x
-./test terse gateways 64 chunk_size 128 ngs 128 duration 24 penalty 20000 seed 5791 | tee $name.txt
-set +x
-[ -f $name.tgz ] && rm $name.tgz
-tar zcf $name.tgz $name.txt log.csv
-#############
-name="gw256c128K"
-#############
-[ -f $name.txt ] && rm $name.txt
-set -x
-./test terse gateways 256 chunk_size 128 ngs 128 duration 24 penalty 20000 seed 5791 | tee $name.txt
-set +x
-[ -f $name.tgz ] && rm $name.tgz
-tar zcf $name.tgz $name.txt log.csv
-#############
-name="gw512c128K"
-#############
-[ -f $name.txt ] && rm $name.txt
-set -x
-./test terse gateways 512 chunk_size 128 ngs 128 duration 24 seed 5791 | tee $name.txt
-set +x
-[ -f $name.tgz ] && rm $name.tgz
-tar zcf $name.tgz $name.txt log.csv
+./do_test.sh gw64ng128c4K 64 4 128 20000 400 
+./do_test.sh gw256ng128c4K 256 /4 128 20000 400 
+./do_test.sh gw64ng128c128K 64 128 128 20000 400 
+./do_test.sh gw256ng128c128K 256 128 128 20000 400
+./do_test.sh gw512ng128c4K 512 4 128 20000 400 
+./do_test.sh gw512ng128c128K 512 128 128 20000 400
+./do_test.sh gw64ng128c4Knl 64 4 128 20000 0
+./do_test.sh gw256ng128c4Knl 256 4 128 20000 0
+./do_test.sh gw64ng128c128Knl 64 128 128 20000 0
+./do_test.sh gw256ng128c128Knl 256 128 128 20000 0
+./do_test.sh gw512ng128c4Knl 512 4 128 20000 0
+./do_test.sh gw512ng128c128Knl 512 128 128 20000 0
+./do_test.sh gw64ng256c64 64 4 256 20000 400
+./do_test.sh gw256ng256c4K 256 4 256  20000 400
+./do_test.sh gw64ng128c256K 64 128 256 20000 400
+./do_test.sh gw256ng256c128K 256 256 128 20000 400
+./do_test.sh gw512ng256c4K 512 4 256 20000 400 
+./do_test.sh gw512ng256c128K 512 128 256 20000 400
+./do_test.sh gw64ng256c4Knl 64 4 256 20000 0
+./do_test.sh gw256ng256c4Knl 256 4 256 20000 0
+./do_test.sh gw64ng256c128Knl 64 128 256 20000 0
+./do_test.sh gw256ng256c128Knl 256 128 256 20000 0
+./do_test.sh gw512ng256c4Knl 512 4 256 20000 0
+./do_test.sh gw512ng256c128Knl 512 128 256 20000 0
+./do_test.sh gw64ng128c4Knp 64 4 128 0 400
+./do_test.sh gw256ng128c4Knp 2564 128 0 400 
+./do_test.sh gw64ng128c128Knp 64 128 128 0 400 
+./do_test.sh gw256ng128c128Knp 256 128 128 0 400
+./do_test.sh gw512ng128c4Knp 512 4 128 0 400 
+./do_test.sh gw512ng128c128Knp 512 128 128 0 400
+./do_test.sh gw64ng128c4Knlnp 64 4 128 0 0 
+./do_test.sh gw256ng128c4Knlnp 256 4 128 0 0 
+./do_test.sh gw64ng128c128Knlnp 64 128 128 0 0 
+./do_test.sh gw256ng128c128Knlnp 256 128 128 0 0
+./do_test.sh gw512ng128c4Knlnp 512 4 128 0 0 
+./do_test.sh gw512ng128c128Knlnp 512 128 128 0 0 
+./do_test.sh gw64ng256c64np 64 4 256 0 400 
+./do_test.sh gw256ng256c4Knp 256 4 256  0 400 
+./do_test.sh gw64ng128c256Knp 64 128 256 0 400 
+./do_test.sh gw256ng256c128Knp 256 256 128 0 400
+./do_test.sh gw512ng256c4Knp 512 4 256 0 400 
+./do_test.sh gw512ng256c128Knp 512 128 256 0 400
+./do_test.sh gw64ng256c4Knlnp 64 4 256 0 0 
+./do_test.sh gw256ng256c4Knlnp 256 4 256 0 0 
+./do_test.sh gw64ng256c128Knlnp 64 128 256 0 0 
+./do_test.sh gw256ng256c128Knlnp 256 128 256 0 0
+./do_test.sh gw512ng256c4Knlnp 512 4 256 0 0 
+./do_test.sh gw512ng256c128Knlnp 512 128 256 0 0 
+
