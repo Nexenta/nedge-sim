e4ca87a0c3509f696673b430390fb909f094a05d
cait <User@Macintosh.local>
2015-08-28 11:43:31 -0700

    chtcp --> chucast

--- do_test.sh
+++ do_test.sh
@@ -9,7 +9,7 @@ gateway_mbs=$6
 [ -f $name.txt ] && rm $name.txt
 set -x
 ./test terse gateways $gws chunk_size $chunk_size ngs $ngs penalty $penalty gateway_mbs $gateway_mbs mbs 1200 seed 5791 duration 50 outprefix $name."rep" rep | tee $name.rep.txt
-./test terse gateways $gws chunk_size $chunk_size ngs $ngs penalty $penalty gateway_mbs $gateway_mbs mbs 1200 seed 5791 duration 50 outprefix $name."chtcp" chtcp | tee $name.chtcp.txt
+./test terse gateways $gws chunk_size $chunk_size ngs $ngs penalty $penalty gateway_mbs $gateway_mbs mbs 1200 seed 5791 duration 50 outprefix $name."chucast" chucast | tee $name.chucast.txt
 ./test terse gateways $gws chunk_size $chunk_size ngs $ngs penalty $penalty gateway_mbs $gateway_mbs mbs 1200 seed 5791 duration 50 outprefix $name."omhtcp" omhucast | tee $name.omhtcp.txt
 ./test terse gateways $gws chunk_size $chunk_size ngs $ngs penalty $penalty gateway_mbs $gateway_mbs mbs 1200 seed 5791 duration 50 outprefix $name."omhudp" omhmcast | tee $name.omhudp.txt
 
