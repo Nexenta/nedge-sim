933dcad5b8e98753fd30114bbc9e43f0aaa62d01
cait <User@Macintosh.local>
2015-09-23 09:38:04 -0700

    Commit benchmark script for FAST16 paper.

---
+++ do_FAST16_benchmarks.sh
@@ -0,0 +1,9 @@
+./sept15_test.sh 100 100 128
+./sept15_test.sh 100 100 4
+./sept15_test.sh 200 100 128
+./sept15_test.sh 200 100 4
+./sept15_test.sh 300 100 128
+./sept15_test.sh 300 100 4
+./sept15_test.sh 900 100 128
+./sept15_test.sh 900 100 4
+
